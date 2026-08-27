367cef73e3f0689fa9357b7168c8c2c9b97d6248
gatfeli1-design <gatfeli1@gmail.com>
2026-08-27 01:37:12 -0400

    Create App component with VistaPublica integration

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+import VistaPublica from './components/VistaPublica';
+
+function App() {
+  return (
+    <div className="App">
+      <VistaPublica />
+    </div>
+  );
+}
+
+export default App;
