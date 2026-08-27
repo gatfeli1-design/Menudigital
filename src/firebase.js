c3c4f1b80d423d42f58856670888ca487523bced
gatfeli1-design <gatfeli1@gmail.com>
2026-08-27 09:17:59 -0400

    Clean up firebase.js by removing comments
    Removed comments and adjusted Firebase initialization.

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDq0WffTjW5ndFls3BsY0fpcZIdow4wPJ0",
   authDomain: "menudigitalapp-19772.firebaseapp.com",
@@ -11,5 +9,4 @@ const firebaseConfig = {
   appId: "1:695735779716:web:e5e5907781656990d51dd1"
 };
 
-// Initialize Firebase
 export const db = initializeApp(firebaseConfig);
