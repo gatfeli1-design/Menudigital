bd6d4a5bfa99dec72351ad028ed964d3b013f644
gatfeli1-design <gatfeli1@gmail.com>
2026-08-27 09:56:32 -0400

    Remove Firebase initialization
    Remove Firebase initialization code from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,1 @@
-import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDq0WffTjW5ndFls3BsY0fpcZIdow4wPJ0",
-  authDomain: "menudigitalapp-19772.firebaseapp.com",
-  projectId: "menudigitalapp-19772",
-  storageBucket: "menudigitalapp-19772.firebasestorage.app",
-  messagingSenderId: "695735779716",
-  appId: "1:695735779716:web:e5e5907781656990d51dd1"
-};
-
-export const db = initializeApp(firebaseConfig);
